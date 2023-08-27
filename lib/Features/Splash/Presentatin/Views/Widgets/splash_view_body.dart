ad827bcff2fbcef6d758919c176992d0632d53e0
Alaa Rashed <alaa.noman.rashed@gmail.com>
2023-08-27 10:25:53 +0300

    Splash view body done

--- lib/Features/Splash/Presentatin/Views/Widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentatin/Views/Widgets/splash_view_body.dart
@@ -1,14 +1,52 @@
+import 'package:bookly_app/Core/Utils/assets_data.dart';
+import 'package:bookly_app/Features/Splash/Presentatin/Views/Widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
 
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(
+            vsync: this,
+            duration: const Duration(seconds: 1),
+        );
+    slidingAnimation = Tween<Offset> (
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    super.initState();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height:4,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/Splash/Presentatin/Views/splash_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
- return const GetMaterialApp(
+ return  GetMaterialApp(
    debugShowCheckedModeBanner: false,
-   home: SplashView(),
+   home: const SplashView(),
+   theme: ThemeData.dark().copyWith(
+     scaffoldBackgroundColor: kPrimaryColor,
+   ),
  );
   }
 
