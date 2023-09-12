8725d4d66a91fe57cb6bbd153a2b2c20b16d4586
Alaa Rashed <alaa.noman.rashed@gmail.com>
2023-09-12 13:21:37 +0300

    Either Class

--- lib/Features/Home/Domain/Repositories/home_repository.dart
+++ lib/Features/Home/Domain/Repositories/home_repository.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/Core/Errors/failure_class.dart';
 import 'package:bookly_app/Features/Home/Domain/Entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepository{
-Future<List<BookEntity>> fetchFeaturedBooks();
-Future<List<BookEntity>> fetchNewsBooks();
+Future<Either< FailureClass, List<BookEntity>>> fetchFeaturedBooks();
+Future<List<BookEntity>> fetchNewestBooks();
 }
