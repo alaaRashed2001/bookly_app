d7a7ba0746db7f13768d7e9cb0ad7155358423a8
Alaa Rashed <alaa.noman.rashed@gmail.com>
2023-09-12 13:05:01 +0300

    Book entity

---
+++ lib/Features/Home/Domain/Entities/book_entity.dart
@@ -0,0 +1,15 @@
+ class BookEntity{
+  final String imgUrl;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(
+      this.imgUrl,
+      this.title,
+      this.authorName,
+      this.price,
+      this.rating,
+      );
+}
