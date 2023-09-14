7e942d6889c1167cb5e7bccb7dc5f4f281386769
Alaa Rashed <alaa.noman.rashed@gmail.com>
2023-09-14 23:13:55 +0300

    Use Case => Domain layer

---
+++ lib/Features/Home/Domain/UseCases/fetch_newest_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/Core/Errors/failure_class.dart';
+import 'package:bookly_app/Core/UseCases/use_cases.dart';
+import 'package:bookly_app/Features/Home/Domain/Entities/book_entity.dart';
+import 'package:bookly_app/Features/Home/Domain/Repositories/home_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepository homeRepository;
+  FetchNewestBooksUseCase(this.homeRepository);
+
+  @override
+  Future<Either<FailureClass, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepository.fetchNewestBooks();
+  }
+}
