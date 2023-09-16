cdaaab105f6a982dd9000103f45141bd312da121
Alaa Rashed <alaa.noman.rashed@gmail.com>
2023-09-16 10:26:53 +0300

    Book Model

--- lib/Features/Home/Domain/Entities/book_entity.dart
+++ lib/Features/Home/Domain/Entities/book_entity.dart
@@ -1,15 +1,17 @@
- class BookEntity{
-  final String imgUrl;
+class BookEntity {
+  final String bookId;
+  final String? imgUrl;
   final String title;
-  final String authorName;
-  final num price;
-  final num rating;
+  final String? authorName;
+  final num? price;
+  final num? rating;
 
-  BookEntity(
-      this.imgUrl,
-      this.title,
-      this.authorName,
-      this.price,
-      this.rating,
-      );
+  BookEntity({
+    required this.bookId,
+    required this.imgUrl,
+    required this.title,
+    required this.authorName,
+    required this.price,
+    required this.rating,
+  });
 }
